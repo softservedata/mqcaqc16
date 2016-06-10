156871d615a4c9c5e0be2b6083a4f0574aafccf1
Yaroslav <garasym@gmail.com>
2016-06-10 10:40:09 +0300

    Convert to Maven Project

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,14 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		System.out.println("Running . . . ");
+		Assert.assertTrue(true);
+	}
+	
+}
